006fc68587ec5b6e0399bd52058539c86fd50481
Mo Khaleel <31739508+MoKhaleel@users.noreply.github.com>
2021-04-15 00:11:16 -0500

    Add files via upload

---
+++ src/NeuralNetworks/ResNet50.py
@@ -0,0 +1,47 @@
+from tensorflow.keras import layers, models
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.applications.resnet50 import ResNet50
+
+from src.Utils.utils import custom_weights_init
+from src.VisualConcept.vcl import VCL
+
+
+def ResNet_50(num_classes, num_vc=0, dropout_rate=0, layer_to_vcl=5, p_h=1, p_w=1, total_sub_models_num=5):
+    m = ResNet50(classes=num_classes, input_shape=(224, 224, 3), weights='imagenet', include_top=False)
+    layer_to_vcl_dict = {1: 'activation_6', 2: 'activation_18', 3: 'activation_33', 4: 'activation_39', 5: 'activation_48'}
+    layer_ = m.get_layer(layer_to_vcl_dict[layer_to_vcl]).output
+
+    if num_vc > 0:
+        layer_ = layers.Dropout(rate=dropout_rate, name='dropout')(layer_)
+        additional_layer_1 = layers.Conv2D(128, (1, 1), activation='relu', padding='same', name='additional_layer_1')(layer_)
+        additional_layer_2 = layers.Conv2D(128, (1, 1), activation='sigmoid', padding='same', name='additional_layer_2')(additional_layer_1)
+        vcl = VCL(num_vcl=num_vc, p_h=p_h, p_w=p_w)(additional_layer_2)
+        output = layers.Dense(num_classes,
+                              activation='softmax',
+                              name='sub_model_predictions',
+                              kernel_initializer=custom_weights_init(weights_shape=[vcl[1].shape[-1], num_classes]),
+                              trainable=False,
+                              use_bias=False)(vcl[0])
+        vcl_next = None
+        if layer_to_vcl == total_sub_models_num:
+            model = models.Model(inputs=m.input, outputs=output, name='ResNet50')
+        else:
+            layer_to_next_vcl = m.get_layer(layer_to_vcl_dict[layer_to_vcl + 1]).output
+            additional_layer_1_next = layers.Conv2D(128, (1, 1), activation='relu', padding='same', name='additional_layer_1_next')(layer_to_next_vcl)
+            additional_layer_2_next = layers.Conv2D(128, (1, 1), activation='sigmoid', padding='same', name='additional_layer_2_next')(additional_layer_1_next)
+            vcl_next = VCL(num_vcl=num_vc, p_h=p_h, p_w=p_w)(additional_layer_2_next)
+            output2 = layers.Dense(num_classes,
+                                   activation='softmax',
+                                   name='sub_model_predictions2',
+                                   kernel_initializer=custom_weights_init(weights_shape=[vcl[1].shape[-1], num_classes]),
+                                   trainable=False,
+                                   use_bias=False)(vcl_next[0])
+            model = models.Model(inputs=m.input, outputs=[output, output2], name='ResNet50')
+        return model, vcl, m.input, vcl_next
+
+    else:
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(m.layers[-1].output)
+        x = layers.Dropout(rate=dropout_rate)(x)
+        output = layers.Dense(num_classes, activation='softmax', name='fc1000', kernel_regularizer=l2(0.001))(x)
+        model = models.Model(inputs=m.input, outputs=output, name='ResNet50')
+        return model
